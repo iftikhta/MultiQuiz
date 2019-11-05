74b2eae9c98fd6412f2dd200d1c50f5ebe304624
tahaiftikhar <t-w-i@hotmail.com>
2019-11-04 21:48:03 -0500

    Working try again option as well, only wrong answer fragment left to do

--- app/src/main/java/sheridan/iftikhar/assignment4/TryAgainFragment.java
+++ app/src/main/java/sheridan/iftikhar/assignment4/TryAgainFragment.java
@@ -12,6 +12,7 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,9 +22,14 @@ import android.widget.TextView;
  */
 public class TryAgainFragment extends Fragment {
 
+    Button btnSee, btnSubmit;
+    NavController mNavController;
     TextView tvNum1, tvNum2;
     EditText edtAnswer;
-    NavController mNavController;
+
+    int correctAnswer;
+
+
     public TryAgainFragment() {
         // Required empty public constructor
     }
@@ -40,6 +46,62 @@ public class TryAgainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mNavController = Navigation.findNavController(view);
+        tvNum1 = view.findViewById(R.id.tvNum1);
+        tvNum2 = view.findViewById(R.id.tvNum2);
+        btnSee = view.findViewById(R.id.btnSee);
+        btnSubmit = view.findViewById(R.id.btnSubmit);
+        edtAnswer = view.findViewById(R.id.edtAnswer);
 
+        tvNum1.setText(Integer.toString(getArguments().getInt("num1")));
+        tvNum2.setText(Integer.toString(getArguments().getInt("num2")));
+        correctAnswer= getArguments().getInt("correctAnswer");
+
+        btnSee.setOnClickListener(v-> see());
+        btnSubmit.setOnClickListener(v -> submit());
+
+    }
+
+    void see (){
+        getArguments().putInt("correctAnswer", correctAnswer);
+        getArguments().putInt("num1", Integer.parseInt(tvNum1.getText().toString()));
+        getArguments().putInt("num2", Integer.parseInt(tvNum2.getText().toString()));
+
+        mNavController.navigate(R.id.action_tryAgainFragment_to_seeAnswerFragment, getArguments());
+
+    }
+
+
+    private void submit() {
+
+        getArguments().putInt("correctAnswer", correctAnswer);
+        getArguments().putInt("num1", Integer.parseInt(tvNum1.getText().toString()));
+        getArguments().putInt("num2", Integer.parseInt(tvNum2.getText().toString()));
+        getArguments().putString("userInp", edtAnswer.getText().toString());
+
+        int len = edtAnswer.getText().toString().length();
+        Boolean isNum = true;
+        for (int i = 0; i < len; i++){
+            if(Character.isDigit(edtAnswer.getText().toString().charAt(i)) == false){
+                isNum = false;
+            }
+        }
+
+        if(!isNum){
+            invalidInput();
+        }
+        else if(edtAnswer.getText() == null){
+            invalidInput();
+        }
+        else if (edtAnswer.getText().toString().equals("")){
+            invalidInput();
+        }
+        //if input is valid do this
+        else if (Integer.parseInt(edtAnswer.getText().toString()) == correctAnswer){
+            mNavController.navigate(R.id.action_tryAgainFragment_to_rightAnswerFragment, getArguments());
+        }
+    }
+
+    void invalidInput(){
+        mNavController.navigate(R.id.action_tryAgainFragment_to_inputErrorFragment, getArguments());
     }
 }
